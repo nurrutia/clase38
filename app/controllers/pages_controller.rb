305a327a1892e1d9307b0c8a4cde88672fa47d2e
Nicolas Urrutia <nurrutialeuno@gmail.com>
2017-06-19 20:37:05 -0400

    guardad datos formulario

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,12 @@ class PagesController < ApplicationController
   end
 
   def new_user
-	params [:nombre ]
+	
+	if params[:nombre].present? && params[:email].present? && params[:edad].present?
+  	@user = User.new(nombre: params[:nombre], email: params[:email], edad: params[:edad]).save
+  	redirect_to pages_x_path, alert:" Listo"
+  	else
+  	redirect_to pages_x_path, alert:" Ingrese datos"
+  	end
   end
 end
